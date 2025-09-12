e464e5c31312ddf2f3ee632693dc7ddcb003cd57
LovelyJacob <57332486+jacobhumston@users.noreply.github.com>
2025-09-12 14:14:20 +0000

    feat: completed first endpoint

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,20 @@
-import { routes } from './url';
+import { routes } from './url.js';
+import { endpoints } from './endpoints.js';
+import type { BadgeId, UserId } from './types.js';
+
+/**
+ * An array of checked badges.
+ */
+export type CheckedBadges = Array<
+    { id: number; owned: false; earnedDate: null } | { id: number; owned: true; earnedDate: Date }
+>;
 
 /** TowerStats API client. */
 export class TowerStatsClient {
     /** API key of this client. */
     #apiKey: string;
+
+    /** API routes of this client. */
     #routes: typeof routes;
 
     /**
@@ -37,5 +48,25 @@ export class TowerStatsClient {
      */
     getRoutes(): typeof routes {
         return this.#routes;
+    }
+
+    /**
+     * Check a user's badges.
+     * @param userId The Roblox user ID.
+     * @param badgeIds An array of badge IDs to check if the user owns them.
+     * @returns An array of checked badge objects specifying whether the user owns that badge or not. (If they do, it includes the date of which they earned the badge.)
+     */
+    async checkBadges(userId: UserId, badgeIds: Array<BadgeId>): Promise<CheckedBadges> {
+        const response = await endpoints.badges(this.#apiKey, userId, badgeIds, this.#routes);
+        const results: CheckedBadges = [];
+        for (const id of badgeIds) {
+            const data = response.find((b) => b[0] === id);
+            if (data && typeof data[1] === 'string') {
+                results.push({ id, owned: true, earnedDate: new Date(data[1]) });
+            } else {
+                results.push({ id, owned: false, earnedDate: null });
+            }
+        }
+        return results;
     }
 }
--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,6 +1,13 @@
-import type { BadgeId, UserId } from './types';
-import { routes } from './url';
+import type { BadgeId, UserId } from './types.js';
+import { routes } from './url.js';
 
+/**
+ * Convenience function for making POST requests to the API.
+ * @param apiKey Your API key.
+ * @param url The endpoint URL.
+ * @param data Data to post.
+ * @returns The fetch response.
+ */
 export async function postRequest(apiKey: string, url: URL, data: any) {
     return fetch(url, {
         method: 'POST',
@@ -11,12 +18,33 @@ export async function postRequest(apiKey: string, url: URL, data: any) {
     });
 }
 
-export async function checkBadges(
+/**
+ * Badges response array.
+ * Each array in the main array contains `[0]<badgeId>` and `[1]<completionDate>`.
+ * Note that unowned badges are not included in the array.
+ */
+export type BadgesResponse = Array<[number, string]>;
+
+/**
+ * Submit badge data for a Roblox user.
+ * @param apiKey Your API key.
+ * @param id Roblox user ID.
+ * @param badges An array of Roblox badge IDs.
+ * @param apiRoutes Optional API route constructor.
+ * @returns
+ */
+async function badges(
     apiKey: string,
     id: UserId,
     badges: Array<BadgeId>,
     apiRoutes: typeof routes = routes
-) {
+): Promise<BadgesResponse> {
     const res = await postRequest(apiKey, apiRoutes.badges, { id, badges });
-    return res.json();
+    if (!res.ok) throw new Error(`Error ${res.status}: ${await res.text()}`);
+    return (await res.json()) as BadgesResponse;
 }
+
+/**
+ * API endpoint functions.
+ */
+export const endpoints = { badges };
